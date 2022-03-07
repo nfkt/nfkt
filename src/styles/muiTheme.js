3a8e4288d605102f990cb1fd7507e1d757b9a1d1
Farhan <nihalfarhankt101@gmail.com>
2022-03-07 17:27:25 +0530

    major changes to the page

--- src/styles/muiTheme.js
+++ src/styles/muiTheme.js
@@ -3,13 +3,15 @@ import { createTheme } from "@mui/material";
 export const muiTheme = createTheme({
   palette: {
     primary: {
-      main: "#5CDB95",
-    },
-    secondary: {
+      // main: "#5CDB95",
       main: "#05386B",
     },
-    mainText:{
-      main: "#EDF5E1"
-    }
+    secondary: {
+      // main: "#05386B",
+      main:"#5CDB95"
+    },
+    mainText: {
+      main: "#EDF5E1",
+    },
   },
 });
